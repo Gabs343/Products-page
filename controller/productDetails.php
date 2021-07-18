9571845b3e9c1f6688d04040795ec15b4a64808e
Gabs343 <gabriellopezp343@gmail.com>
2021-07-18 17:48:05 -0300

    añadiendo los datos del campo al array

--- controller/productDetails.php
+++ controller/productDetails.php
@@ -31,6 +31,7 @@
                 $this->view->setEspec = $this->isSubmit("editEspecificacion");
                 $this->view->mostrarEspec = $this->isSubmit("mostrarEspec");
                 $this->view->codes = $this->modelo->getPermisosCodes();
+                $this->view->campo = $this->isSubmit("setCampo");
                 $this->view->render("productDetails/index_emp");
 
             }else{
@@ -185,6 +186,20 @@
         if ($exito) {
             echo "<meta http-equiv='refresh' content='0'>";
         } 
+    }
+
+    public function setCampo(){
+        $campo = array(
+            "label" => $_POST["label"],
+            "tipo" => $_POST["tipo"],
+            "requerido" => intval($_POST["requerido"]),
+            "opcion" => $_POST["opcion"],
+            "ID_Producto" => intval($_GET["id"])
+        );
+        $exito = $this->modelo->setCampoDinamico($campo);
+        if($exito){
+            echo "true";
+        }
     }
     
 }
