076203dbee79493100df3c9deab7af37ccc92083
Gabs343 <gabriellopezp343@gmail.com>
2021-07-31 22:54:11 -0300

    trayendo las consultas realizadas por el cliente de la BD

--- model/contacto_modelo.php
+++ model/contacto_modelo.php
@@ -5,6 +5,24 @@ class ContactoModelo extends Model
     {
         parent::__construct();
     }
+
+    public function getConsult(){
+        $consultas = [];
+        
+        try{
+            $query = "SELECT * FROM contacto";
+
+            $con = $this->db->connect();
+            $con = $con->query($query);
+                    
+            while($row = $con->fetch(PDO::FETCH_ASSOC)){
+                array_push($consultas, $row); 
+            }
+            return $consultas;
+        }catch(PDOException $e){
+            return [];
+        }
+    }
 }
 
 ?>
