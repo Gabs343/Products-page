a819ab787c7901a2dc45a9de9150f4b113272215
Gabs343 <gabriellopezp343@gmail.com>
2021-07-31 23:18:10 -0300

    guardando los campos de la consulta en un array para insertarlo en la BD

--- controller/contacto.php
+++ controller/contacto.php
@@ -14,11 +14,24 @@
                 $this->view->render("contacto/index_emp");
 
             }else{
-
+                $this->view->sendConsulta = $this->isSubmit("sendConsulta");
                 $this->view->render("contacto/index");
 
             }
             
+        }
+
+        public function sendConsulta(){
+            $consulta = array(
+                "nombre" => $_POST["nombre"],
+                "apellido" => $_POST["apellido"],
+                "email" => $_POST["email"],
+                "celular" => $_POST["celular"],
+                "area" => $_POST["area"],
+                "mensaje" => $_POST["mensaje"] 
+            );
+
+            $exito = $this->modelo->insertConsulta($consulta);
         }
 
     }
