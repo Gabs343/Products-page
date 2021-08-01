3de0dc13e169640f8a91a0d68ac6498ce66ee4e5
Gabs343 <gabriellopezp343@gmail.com>
2021-07-31 23:01:28 -0300

    tabla con las consultas del usuario

--- view/contacto/index_emp.php
+++ view/contacto/index_emp.php
@@ -1,2 +1,24 @@
 <?php require "view/header.php"; ?>
+<table>
+    <thead>
+        <th>Nombre</th>
+        <th>Apellido</th>
+        <th>Correo</th>
+        <th>Celular</th>
+        <th>Area</th>
+        <th>Mensaje</th>
+    </thead>
+    <tbody>
+        <?php foreach($this->consultas as $clave){ ?>
+            <tr>
+                <td><?php echo $clave["nombre"]; ?></td>
+                <td><?php echo $clave["apellido"]; ?></td>
+                <td><?php echo $clave["email"]; ?></td>
+                <td><?php echo $clave["celular"]; ?></td>
+                <td><?php echo $clave["area"]; ?></td>
+                <td><?php echo $clave["mensaje"]; ?></td>
+            </tr>
+        <?php } ?>
+    </tbody>
+</table>
 <?php require "view/footer.php"; ?>
